04c8b2e013d9f6c5efd79444835c71ab33047b62
Gulunsal <gulunsal@example.com>
2025-02-11 13:15:37 +0300

    validation password check

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,18 +1,19 @@
 const createError = require('http-errors');
 
 const validateBody = schema => {
-  const func = async (req, res, next) => {
+  return async (req, res, next) => {
     try {
       const { error } = schema.validate(req.body);
+      
       if (error) {
-        throw createError(400, error.details[0].message);
+        throw createError(400, error.message);
       }
+      
       next();
     } catch (error) {
       next(error);
     }
   };
-  return func;
 };
 
 module.exports = validateBody; 
