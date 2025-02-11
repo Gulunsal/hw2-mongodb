2ff300cfd59705132c08f1e9c4a1c8106164de3c
Gulunsal <gulunsal@example.com>
2025-02-11 09:10:31 +0300

    contact validate problem

--- src/schemas/contact.js
+++ src/schemas/contact.js
@@ -1,20 +1,16 @@
 const Joi = require('joi');
 
 const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().required(),
-  email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().required()
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required()
 });
 
 const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string(),
+  name: Joi.string(),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-}).min(1);
+  phone: Joi.string()
+});
 
 module.exports = {
   createContactSchema,
